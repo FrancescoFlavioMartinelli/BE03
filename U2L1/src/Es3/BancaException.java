76e363adecbd45b71b244b3d05304830b7665ddf
FrancescoFlavioMartinelli <98541373+FrancescoFlavioMartinelli@users.noreply.github.com>
2022-10-03 18:17:29 +0200

    Add files via upload

---
+++ U2L1/src/Es3/BancaException.java
@@ -0,0 +1,19 @@
+package Es3;
+
+public class BancaException extends Exception {
+	
+	private String msg;
+	
+	public BancaException(String m) {
+		super(m);
+		this.msg = m;
+	}
+	
+	@Override
+	public String toString() {
+		return "BANCAEXCEPTION - " + this.msg;
+	}
+	
+	
+
+}
